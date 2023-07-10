813f9d5b6eebc46f559c2b9679b23d48ed0cebbb
pjh0904 <pjh0904@jainwon.com>
2023-07-10 15:30:41 +0900

    feat: 뺄셈 계산을 할 수 있는 함수와 코테스트 작성

--- src/test/kotlin/StringCalculatorTest.kt
+++ src/test/kotlin/StringCalculatorTest.kt
@@ -13,6 +13,17 @@ class StringCalculatorTest : FunSpec({
         // then
         answer shouldBe 6
     }
+
+    test("뺄셈만 있는 문자열 식을 받아 계산할 수 있다.") {
+        // given
+        val expression = "2 - 3 - 1"
+
+        // when
+        val answer = calculate(expression)
+
+        // then
+        answer shouldBe -2
+    }
 })
 
 fun calculate(expression: String): Int {
@@ -20,11 +31,12 @@ fun calculate(expression: String): Int {
     var answer = splitExpression[0].toInt()
 
     for (index in 1 until splitExpression.size step 2) {
-        val operator = splitExpression[index]
+        val operator = splitExpression[index].trim()
         val number = splitExpression[index + 1].toInt()
 
         when (operator) {
             "+" -> answer = answer plus number
+            "-" -> answer = answer minus number
         }
     }
 
@@ -33,4 +45,8 @@ fun calculate(expression: String): Int {
 
 infix fun Int.plus(other: Int): Int {
     return this.plus(other)
+}
+
+infix fun Int.minus(other: Int): Int {
+    return this.minus(other)
 }
