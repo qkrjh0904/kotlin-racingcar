36918c9df4063293b2b4db26b91f487bd38d98e3
pjh0904 <pjh0904@jainwon.com>
2023-07-10 14:53:34 +0900

    refactor: 코드리뷰 적용해 named argument 사용하도록 변경

--- src/main/kotlin/Person.kt
+++ src/main/kotlin/Person.kt
@@ -2,8 +2,4 @@ data class Person(
     val name: String,
     val age: Int,
     var nickname: String?
-) {
-
-    constructor(name: String) : this(name, 29, null) {
-    }
-}
+)
--- src/test/kotlin/PersonTest.kt
+++ src/test/kotlin/PersonTest.kt
@@ -4,23 +4,27 @@ import io.kotest.matchers.shouldBe
 
 class PersonTest : FunSpec({
 
-    test("Person 에 이름을 넣어 객체를 생성해본다.") {
-        val person = Person("박정호")
+    test("Named Argument 사용해서 객체 생성하기") {
+        val person = Person(name = "박정호", age = 29, nickname = "바저호")
 
         person.name shouldBe "박정호"
+        person.age shouldBe 29
+        person.nickname shouldBe "바저호"
     }
 
-    test("Person 에 null 타입일 수 있는 인자에 대해 테스트 해본다.") {
-        val person = Person("박정호")
+    test("Argument 중 null 타입일 수 있는 argument 에 대해 테스트해본다.") {
+        val person = Person(name = "박정호", age = 29, nickname = null)
 
         person.name shouldBe "박정호"
+        person.age shouldBe 29
         person.nickname shouldBe null
     }
 
-    test("Person을 데이터 클래스로 만들어 보고 정상 동작 하는지 확인해본다.") {
-        val person = Person("박정호")
+    test("Person 을 데이터 클래스로 만들어 보고 정상 동작 하는지 확인해본다.") {
+        val person = Person(name = "박정호", age = 29, nickname = null)
 
         person.name shouldBe "박정호"
+        person.age shouldBe 29
         person.nickname shouldBe null
     }
 })
