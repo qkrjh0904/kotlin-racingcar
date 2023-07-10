642b223a35370d6a982b7a2d4af1476038c8531d
pjh0904 <pjh0904@jainwon.com>
2023-07-10 11:58:34 +0900

    feat: step1에 대한 kotest 작성

--- src/main/kotlin/Person.kt
+++ src/main/kotlin/Person.kt
@@ -1,1 +1,9 @@
-class Person(val name: String, val age: Int, var nickname: String)
+data class Person(
+    val name: String,
+    val age: Int,
+    var nickname: String?
+) {
+
+    constructor(name: String) : this(name, 29, null) {
+    }
+}
