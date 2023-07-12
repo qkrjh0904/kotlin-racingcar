ca1322e91cd5344df02737d25223f6d144bfb80c
pjh0904 <pjh0904@jainwon.com>
2023-07-12 10:04:18 +0900

    refactor: 연산자 체크 책임 Operator 로 변경

--- src/main/kotlin/enums/Operator.kt
+++ src/main/kotlin/enums/Operator.kt
@@ -1,8 +1,18 @@
 package enums
 
-enum class Operator(val operator: String) {
+enum class Operator(
+    val operator: String
+) {
     PLUS("+"),
     MINUS("-"),
     TIMES("*"),
-    DIV("/")
+    DIV("/");
+
+    companion object {
+
+        private val operatorMap = values().associateBy { it.operator }
+        fun of(operator: String): Operator = operatorMap.getOrElse(operator) {
+            throw IllegalArgumentException("${ErrorMessage.NOT_A_OPERATOR.message} operator=$operator")
+        }
+    }
 }
--- src/main/kotlin/objects/StringCalculator.kt
+++ src/main/kotlin/objects/StringCalculator.kt
@@ -4,7 +4,7 @@ import enums.ErrorMessage.CANNOT_DIVIDE_BY_ZERO
 import enums.ErrorMessage.EXPRESSION_CANNOT_BE_EMPTY
 import enums.ErrorMessage.EXPRESSION_COUNT_SHOULD_BE_ODD
 import enums.ErrorMessage.NOT_A_NUMBER
-import enums.ErrorMessage.NOT_A_OPERATOR
+import enums.Operator
 import enums.Operator.DIV
 import enums.Operator.MINUS
 import enums.Operator.PLUS
@@ -29,29 +29,33 @@ class StringCalculator(
     }
 
     fun calculate(): Double {
-        val splitExpression = expression!!.split(DELIMITER_SPACE)
-        splitExpression.map { it.trim() }
+        val splitExpression = splitExpression(expression!!)
+        val queue = ArrayDeque(splitExpression)
+
+        var answer = toNumber(queue.removeFirst())
+        while (queue.isNotEmpty()) {
+            val operator = Operator.of(queue.removeFirst())
+            val number = toNumber(queue.removeFirst())
+
+            answer = when (operator) {
+                PLUS -> answer.plus(number)
+                MINUS -> answer.minus(number)
+                TIMES -> answer.times(number)
+                DIV -> answer.checkAndDiv(number)
+            }
+        }
+
+        return answer
+    }
+
+    private fun splitExpression(expression: String): List<String> {
+        val splitExpression = expression.split(DELIMITER_SPACE).map { it.trim() }
 
         require(splitExpression.size % TWO == ONE) {
             EXPRESSION_COUNT_SHOULD_BE_ODD.message
         }
 
-        val queue = ArrayDeque(splitExpression)
-        var answer = toNumber(queue.removeFirst())
-        while (queue.isNotEmpty()) {
-            val operator = queue.removeFirst()
-            val number = toNumber(queue.removeFirst())
-
-            answer = when (operator) {
-                PLUS.operator -> answer.plus(number)
-                MINUS.operator -> answer.minus(number)
-                TIMES.operator -> answer.times(number)
-                DIV.operator -> answer.checkAndDiv(number)
-                else -> throw IllegalArgumentException("${NOT_A_OPERATOR.message} operator=$operator")
-            }
-        }
-
-        return answer
+        return splitExpression
     }
 
     private fun toNumber(number: String): Double {
