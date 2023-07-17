9ed4a654b5a007b266817de89316c366f816fe30
pjh0904 <pjh0904@jainwon.com>
2023-07-17 15:30:25 +0900

    refactor: mockInput 라인 변경 수정
    - mock input 에서 \n로 라인 변경 되도록 수정

--- src/test/kotlin/CarRacingInputTest.kt
+++ src/test/kotlin/CarRacingInputTest.kt
@@ -5,28 +5,26 @@ import java.io.ByteArrayInputStream
 
 class CarRacingInputTest : BehaviorSpec({
 
-    Given("정상적인 자동차 대수를 입력 한다.") {
-        val mockInput = "3"
+    Given("정상적인 자동차 대수를 입력 하고, 정상적인 시도 횟수를 입력한다.") {
+        val mockInput = "3\n4"
         val inputStream = ByteArrayInputStream(mockInput.toByteArray())
         System.setIn(inputStream)
 
         When("입력을 받고 유효성 체크를 한다.") {
             val racingCarNum = InputDevice.input()
+            val attemptsNum = InputDevice.input()
 
             Then("정상 출력 된다.") {
                 println(racingCarNum)
+                println(attemptsNum)
             }
         }
     }
 
     Given("정상적인 자동차 대수를 입력 하고, 시도 횟수에 0 이하의 숫자를 넣는다.") {
-        val mockInput = "1"
+        val mockInput = "1\n-1"
         val inputStream = ByteArrayInputStream(mockInput.toByteArray())
         System.setIn(inputStream)
-
-        val mockInput2 = "-1"
-        val inputStream2 = ByteArrayInputStream(mockInput2.toByteArray())
-        System.setIn(inputStream2)
 
         When("입력을 받고 유효성 체크를 한다.") {
             Then("IllegalArgumentException 이 발생한다.") {
@@ -40,13 +38,9 @@ class CarRacingInputTest : BehaviorSpec({
     }
 
     Given("정상적인 자동차 대수를 입력 하고, 시도 횟수에 숫자가 아닌 문자열을 넣는다.") {
-        val mockInput = "10"
+        val mockInput = "10\na"
         val inputStream = ByteArrayInputStream(mockInput.toByteArray())
         System.setIn(inputStream)
-
-        val mockInput2 = "a"
-        val inputStream2 = ByteArrayInputStream(mockInput2.toByteArray())
-        System.setIn(inputStream2)
 
         When("입력을 받고 유효성 체크를 한다.") {
             Then("NumberFormatException 이 발생한다.") {
@@ -60,13 +54,9 @@ class CarRacingInputTest : BehaviorSpec({
     }
 
     Given("정상적인 자동차 대수를 입력 하고, 빈 문자열을 넣는다.") {
-        val mockInput = "3"
+        val mockInput = "3\n   "
         val inputStream = ByteArrayInputStream(mockInput.toByteArray())
         System.setIn(inputStream)
-
-        val mockInput2 = "    "
-        val inputStream2 = ByteArrayInputStream(mockInput2.toByteArray())
-        System.setIn(inputStream2)
 
         When("입력을 받고 유효성 체크를 한다.") {
             Then("IllegalArgumentException 이 발생한다.") {
